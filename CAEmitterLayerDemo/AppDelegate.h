a5bce0c540f334b488f8c85688bf62bcd1f5de7e
CityFire <13787697167@163.com>
2015-11-15 17:02:32 +0800

    Initial Commit

---
+++ CAEmitterLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAEmitterLayerDemo
+//
+//  Created by wjc on 15/11/15.
+//  Copyright © 2015年 wjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAEmitterLayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAEmitterLayerDemo
+//
+//  Created by wjc on 15/11/15.
+//  Copyright © 2015年 wjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
